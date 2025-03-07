529d9d88deb64fe7f77c99acaf42c04e139d3def
athenova <aleksey.sergeyev@yandex.com>
2025-03-07 15:36:15 +0300

    Убираем folder_id из кода

--- project.py
+++ project.py
@@ -39,8 +39,8 @@ class Project(CommonBlogger):
             first_post_date=datetime(2025, 3, 9),
             #days_to_review=timedelta(2),
             days_between_posts=timedelta(7),
-            #text_generator=YandexTextGenerator(folder_id="b1g8kgobin3d1usjbiq8"),
-            image_generator=YandexImageGenerator(folder_id="b1g8kgobin3d1usjbiq8"),
+            #text_generator=YandexTextGenerator(),
+            image_generator=YandexImageGenerator(),
             topic_word_limit=100,
             **kwargs
         )
